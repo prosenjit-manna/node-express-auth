ef4d2137373c3d584d85857326572b3db0f2ef11
Prosenjit Manna <prosenjit@itobuz.com>
2024-04-04 17:18:32 +0530

    login and request and response types added

--- src/view/auth.ts
+++ src/view/auth.ts
@@ -3,22 +3,24 @@ import { UserModel } from '../models/User';
 import jwt from 'jsonwebtoken';
 import express, { Request, Response } from 'express';
 import { get_env } from '../lib/get-env';
+import { LoginRequest } from '../apiModel/login/loginRequest.interface';
+import { LoginResponse } from '../apiModel/login/loginResponse.interface';
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
+router.post('/register', async ({ body }: { body: LoginRequest }, res: Response) => {
   try {
-    const { username, password } = req.body;
+    const { username, password } = body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new UserModel({ username, password: hashedPassword });
     await user.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: 'User registered successfully' }) as LoginResponse;
   } catch (error: any) {
-    res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message }) as LoginResponse;
   }
 });
 
 // User login
-router.post('/login', async (req, res) => {
+router.post('/login', async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
     const user = await UserModel.findOne({ username });
@@ -31,7 +33,7 @@ router.post('/login', async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, get_env.JSON_WEB_TOKEN_SECRET, {
-      expiresIn: '1h',
+      expiresIn: get_env.JSON_WEB_TOKEN_EXPIRY,
     });
     res.status(200).json({ token });
   } catch (error) {
